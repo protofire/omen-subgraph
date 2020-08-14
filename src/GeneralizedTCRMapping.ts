9d9cc62d96d136b001f40abba5fbe2d06ab2ba83
mtsalenc <mtsalenc@gmail.com>
2020-08-14 14:38:18 -0300

    feat: use fpmm instead of a new entity

--- src/GeneralizedTCRMapping.ts
+++ src/GeneralizedTCRMapping.ts
@@ -1,7 +1,30 @@
-import { Address, BigInt, log, Bytes } from '@graphprotocol/graph-ts';
+import { log } from '@graphprotocol/graph-ts';
 import { ItemStatusChange, GeneralizedTCR } from '../generated/GeneralizedTCR/GeneralizedTCR';
-import { CuratedMarket } from '../generated/schema';
+import { FixedProductMarketMaker } from '../generated/schema';
 
+function hexStringToLowerCase(input: string): string {
+  // Code looks weird? Unfortunately the current version
+  // of assemblyscript does not support things like regex
+  // and replace/replaceAll, so we work around it.
+  let output = ''
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] == 'A')
+      output += 'a'
+    else if (input[i] == 'B')
+      output += 'b'
+    else if (input[i] == 'C')
+      output += 'c'
+    else if (input[i] == 'D')
+      output += 'd'
+    else if (input[i] == 'E')
+      output += 'e'
+    else if (input[i] == 'F')
+      output += 'f'
+    else output += input[i]
+  }
+
+  return output
+}
 
 export function handleItemStatusChange(event: ItemStatusChange): void {
   const tcr = GeneralizedTCR.bind(event.address);
@@ -10,20 +33,30 @@ export function handleItemStatusChange(event: ItemStatusChange): void {
 
   const addressStartIndex = decodedData.lastIndexOf('0x')
   if (addressStartIndex == -1) {
-    log.warning('No address found for itemID {} ', [event.params._itemID.toHexString()])
+    log.warning('GTCR: No address found for itemID {} ', [event.params._itemID.toHexString()])
     return // Invalid submission. No Op
   }
   const fpmmAddress = decodedData.slice(addressStartIndex, addressStartIndex + 42)
 
-  let curatedMarket = CuratedMarket.load(fpmmAddress)
-  if (curatedMarket == null) {
-    curatedMarket = new CuratedMarket(fpmmAddress);
-    curatedMarket.itemID = event.params._itemID.toHexString();
+  // Workaround missing String.toLowerCase function in assemblyscript.
+  const lowerCaseFpmmAddr = hexStringToLowerCase(fpmmAddress)
+  log.info('GTCR: address input {} address output {} ', [fpmmAddress, lowerCaseFpmmAddr])
+
+  let fpmm = FixedProductMarketMaker.load(lowerCaseFpmmAddr);
+  if (fpmm == null) {
+    log.warning("GTCR: Could not load FPMM for {}", [lowerCaseFpmmAddr]);
+    return;
+  } else {
+    log.info("GTCR: Got FPMM for {}", [lowerCaseFpmmAddr])
   }
 
-  curatedMarket.status = itemInfo.value1;
-  curatedMarket.registered = curatedMarket.status == 1 || curatedMarket.status == 3;
-  curatedMarket.save();
+  fpmm.klerosTCRitemID = event.params._itemID.toHexString();
+  fpmm.klerosTCRstatus = itemInfo.value1;
+  fpmm.klerosTCRregistered = fpmm.klerosTCRstatus == 1 || fpmm.klerosTCRstatus == 3;
+  fpmm.save();
+
+  let a = fpmm.klerosTCRstatus
+  log.info("GTCR: Saved fpmm at {} new status {} itemID {}", [lowerCaseFpmmAddr, a.toString(), event.params._itemID.toHexString()])
 }
 
 
