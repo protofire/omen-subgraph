8a6fa8793967c10b6187d2feea2551fb41c6a64c
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2020-11-03 15:15:45 +0100

    chore(gelato): Fix deployment error

--- migrations/13_deploy_gelato.js
+++ migrations/13_deploy_gelato.js
@@ -2,6 +2,7 @@ const GelatoCore = artifacts.require("GelatoCore");
 const GelatoGasPriceOracle = artifacts.require("GelatoGasPriceOracle");
 
 module.exports = async function(deployer) {
+  await deployer.deploy(GelatoGasPriceOracle, 21)
   await deployer.deploy(GelatoCore, { 
       gelatoGasPriceOracle: GelatoGasPriceOracle.address, 
       oracleRequestData: "0x0",
