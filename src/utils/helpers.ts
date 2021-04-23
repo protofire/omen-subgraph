a6e48e4cbb08ff05e1d1273d5dc4bc41fb633a55
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-04-23 15:17:36 -0700

    Add support for liquidity mining (#154)
    * Update networks.json
    
    * Update schema to include liquidity mining contract types
    
    * Update template files
    
    * handleDistributionInitialization
    
    * handleDistributionCancelation
    
    * handleDeposit
    
    * handleWithdrawal
    
    * handleClaim
    
    * handleRecovery
    
    * Manually retrieve StakingRewardsFactory addresses
    
    * Update helpers
    
    * Update readme to include LiquidityMiningCampaign entity
    
    * Add generated staking rewards mapping file
    
    * Update StakingRewardsFactory address
    
    * Update distribution factory contract address
    
    * Fix references to staking rewards factory address
    
    * Use BigInt instead of BigDecimal for reward and staked amounts
    
    * Remove unused logs
    
    * Fix missing parameter in Initialized event
    
    * Remove unnecessary if else
    
    * Include ERC20Detailed abi in distribution
    
    * Update rinkeby subgraph

---
+++ src/utils/helpers.ts
@@ -0,0 +1,6 @@
+import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
+
+export let ZERO_BI = BigInt.fromI32(0);
+export let ONE_BI = BigInt.fromI32(1);
+export let ZERO_BD = BigDecimal.fromString("0");
+export let BI_18 = BigInt.fromI32(18);
