8a33183a0b507b0b96be62b101b259ef44195fb6
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-05-14 15:19:58 -0700

    Include V2 of fpmm factory contract along with v1

--- render-templates.js
+++ render-templates.js
@@ -20,6 +20,7 @@ module.exports = function (callback) {
 
     for (const contractName of [
       "FPMMDeterministicFactory",
+      "FPMMDeterministicFactoryV2",
       "FixedProductMarketMaker",
       "ConditionalTokens",
       "Realitio",
