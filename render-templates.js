c596bce665b21378a96d828bfc77eff3b81d4f09
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-07-22 11:14:28 -0700

    refactor: remove gelato code (#180)

--- render-templates.js
+++ render-templates.js
@@ -36,7 +36,6 @@ module.exports = function (callback) {
       "DAI",
       "USDC",
       "USDT",
-      "GelatoCore",
       "StakingRewardsFactory",
       "StakingRewardsDistribution",
     ]) {
