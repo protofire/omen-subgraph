77155a396c62e3e32e9d421ec65321ea7b958d31
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-08-10 13:57:16 -0700

    Get fpmm factory address (#171)
    * feature: get fpmm factory address
    
    * fix: set subgraph back to protofire
    
    * fix: convert to hex string instead of string
    
    * fix: properly set fpmm v2 factory address
    
    * feat: update v2 factory address

--- src/FPMMDeterministicFactoryMapping.template.ts
+++ src/FPMMDeterministicFactoryMapping.template.ts
@@ -12,6 +12,7 @@ import { joinDayAndVolume } from "./utils/day-volume";
 import { updateScaledVolumes, setLiquidity } from "./utils/fpmm";
 import { requireToken } from "./utils/token";
 import { requireGlobal } from "./utils/global";
+import { getFPMMDeterministicFactoryV2Address } from "./utils/addresses";
 
 export function handleFixedProductMarketMakerCreation(
   event: FixedProductMarketMakerCreation
@@ -32,6 +33,7 @@ export function handleFixedProductMarketMakerCreation(
 
   fpmm.creator = event.params.creator;
   fpmm.creationTimestamp = event.block.timestamp;
+  fpmm.factory = getFPMMDeterministicFactoryV2Address();
 
   fpmm.collateralToken = event.params.collateralToken;
   fpmm.fee = event.params.fee;
