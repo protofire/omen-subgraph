1751ea8d051507a52139f42fe50aa6c15c96f124
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-08-19 13:48:00 -0700

    fix: update staking factory rinkeby address

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -6,7 +6,7 @@ export function getStakingRewardsFactoryAddress(): string {
   let network = dataSource.network() as string;
   // not using a switch-case because using strings is not yet supported (only u32)
   if (network == "mainnet") return ADDRESS_ZERO;
-  if (network == "rinkeby") return "0x264f2e08859736D96B546d1EF206C9D4dDDef05a";
+  if (network == "rinkeby") return "0xf50d900859da289a34204bC5FcA14c77575BD910";
   if (network == "xdai") return "0x583d56828996060aDF22c4D8d27371FD5b7F637B";
   log.warning("no staking rewards factory address for unsupported network {}", [
     network,
