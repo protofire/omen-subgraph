91e71b3447bc3057d0702be50f7ba8cc7dc03d0a
Kaden Zipfel <kaden.zipfel@hotmail.com>
2021-08-19 14:45:37 -0700

    Revert "Merge branch 'master' into fix/174"
    This reverts commit 3ab2cd893a18ee58a4258daa641dd953b89e49cd, reversing
    changes made to 764ae342cbbfcf2d055cf584ecd9a07ef8b90e58.

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -6,9 +6,21 @@ export function getStakingRewardsFactoryAddress(): string {
   let network = dataSource.network() as string;
   // not using a switch-case because using strings is not yet supported (only u32)
   if (network == "mainnet") return ADDRESS_ZERO;
-  if (network == "rinkeby") return "0xf50d900859da289a34204bC5FcA14c77575BD910";
+  if (network == "rinkeby") return "0x264f2e08859736D96B546d1EF206C9D4dDDef05a";
   if (network == "xdai") return "0x583d56828996060aDF22c4D8d27371FD5b7F637B";
   log.warning("no staking rewards factory address for unsupported network {}", [
+    network,
+  ]);
+  return ADDRESS_ZERO;
+}
+
+export function getFPMMDeterministicFactoryV2Address(): string {
+  let network = dataSource.network() as string;
+  // not using a switch-case because using strings is not yet supported (only u32)
+  if (network == "mainnet") return ADDRESS_ZERO;
+  if (network == "rinkeby") return "0x3A8eDb904ee335Fb828EC71e080d8BfA2CDb8167";
+  if (network == "xdai") return ADDRESS_ZERO;
+  log.warning("no fpmm v2 factory address for unsupported network {}", [
     network,
   ]);
   return ADDRESS_ZERO;
